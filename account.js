4eb5416b5d36caf68316779693a862a0e91f8c39
Radoslav Sharapanov <rado@me.com>
2022-09-08 15:19:10 +0300

    ,

--- account.js
+++ account.js
@@ -1,14 +1,20 @@
 (function() {
 	const init = () => {
-		document.querySelectorAll('button, aside nav a').forEach(el => el.onpointerdown = e => {
-			let el = e.target.closest('button, a');
-			let diameter = Math.max(el.clientWidth, el.clientHeight);
-			let x = e.clientX - el.offsetLeft - diameter;
-			let y = e.clientY - el.offsetTop - diameter;
+		document.querySelectorAll('.btn, aside nav li').forEach(el => el.onpointerdown = e => {
+			let el = e.target.closest('li') || e.target.closest('.btn');
+			let radius = Math.max(el.clientWidth, el.clientHeight);
+			console.log(e.clientX - el.offsetLeft, e.clientY - el.offsetTop);
+			let x = e.clientX - el.offsetLeft;
+			let y = e.clientY - el.offsetTop;
+			// el.style.transitionProperty = 'none';
+			// el.style.setProperty('--ripple-x', `${x}px`);
+			// el.style.setProperty('--ripple-y', `${y}px`);
+			// el.style.transitionProperty = '';
 			el.style.setProperty('--ripple-x', `${x}px`);
 			el.style.setProperty('--ripple-y', `${y}px`);
-			el.style.setProperty('--ripple-diameter', `${diameter * 2}px`);
-			console.log(x, y);
+			el.style.setProperty('--ripple-diameter', `${radius * 2}px`);
+			el.style.setProperty('--ripple-radius', `${radius}px`);
+			console.log(x, y, radius * 2);
 		});
 		document.querySelector('#mobile-menu-trigger')?.addEventListener('change', e => {
 			let main = document.querySelector('main');
@@ -30,7 +36,7 @@
 				toggle.checked = false;
 			}
 		}, { passive: true });
-	}
+	};
 	if (document.readyState !== "loading") {
 		init();
 	} else {
