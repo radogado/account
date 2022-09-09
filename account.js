6439356d2483b8260584a1935cc01e232b2d5bb8
Radoslav Sharapanov <rado@me.com>
2022-09-09 19:38:21 +0300

    ,

--- account.js
+++ account.js
@@ -2,17 +2,18 @@
 	const init = () => {
 		document.querySelectorAll('.btn, aside nav li').forEach(el => el.onpointerdown = e => {
 			let el = e.target.closest('li') || e.target.closest('.btn');
-			let radius = Math.max(el.clientWidth, el.clientHeight);
-			console.log(e.clientX - el.offsetLeft, e.clientY - el.offsetTop);
 			let x = e.clientX - el.offsetLeft;
 			let y = e.clientY - el.offsetTop;
-			// el.style.transitionProperty = 'none';
-			// el.style.setProperty('--ripple-x', `${x}px`);
-			// el.style.setProperty('--ripple-y', `${y}px`);
-			// el.style.transitionProperty = '';
+			let max_x = Math.max(x, el.clientWidth - x);
+			let max_y = Math.max(y, el.clientHeight - y);
+			let radius = Math.sqrt(max_x * max_x + max_y * max_y);
+			// console.log(e.clientX - el.offsetLeft, e.clientY - el.offsetTop);
+			el.style.transitionProperty = 'none';
 			el.style.setProperty('--ripple-x', `${x}px`);
 			el.style.setProperty('--ripple-y', `${y}px`);
-			el.style.setProperty('--ripple-diameter', `${radius * 2}px`);
+			el.style.transitionProperty = '';
+			el.style.setProperty('--ripple-x', `${x}px`);
+			el.style.setProperty('--ripple-y', `${y}px`);
 			el.style.setProperty('--ripple-radius', `${radius}px`);
 			console.log(x, y, radius * 2);
 		});
