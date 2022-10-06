f503526ae9df67804321266b9304529b9a5acc33
Rado <spacest@gmail.com>
2022-10-06 20:21:38 +0300

    ,

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 component=account
-./node_modules/sass/sass.js $component.scss > $component.css
-./node_modules/clean-css-cli/bin/cleancss -o $component.min.css $component.css
+./node_modules/sass/sass.js css/$component.scss > css/$component.css
+./node_modules/clean-css-cli/bin/cleancss -o css/$component.min.css css/$component.css
 ./node_modules/terser/bin/terser -o $component.min.js --compress --mangle --source-map includeSources -- $component.js
-./node_modules/gzip-size-cli/cli.js --raw $component.min.css > $component.min.css.size
+./node_modules/gzip-size-cli/cli.js --raw css/$component.min.css > $component.min.css.size
 ./node_modules/gzip-size-cli/cli.js --raw $component.min.js > $component.min.js.size
