dec101887785f1f6014850a5257a12e8a799b348
Rado <spacest@gmail.com>
2022-10-07 09:02:47 +0300

    ,

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 component=account
 ./node_modules/sass/sass.js css/$component.scss > css/$component.css
-./node_modules/clean-css-cli/bin/cleancss -o css/$component.min.css css/$component.css
-./node_modules/terser/bin/terser -o $component.min.js --compress --mangle --source-map includeSources -- $component.js
-./node_modules/gzip-size-cli/cli.js --raw css/$component.min.css > $component.min.css.size
-./node_modules/gzip-size-cli/cli.js --raw $component.min.js > $component.min.js.size
+./node_modules/clean-css-cli/bin/cleancss -o dist/$component.min.css css/$component.css
+./node_modules/terser/bin/terser -o dist/$component.min.js --compress --mangle --source-map includeSources -- $component.js
+./node_modules/gzip-size-cli/cli.js --raw dist/$component.min.css > $component.min.css.size
+./node_modules/gzip-size-cli/cli.js --raw dist/$component.min.js > $component.min.js.size
